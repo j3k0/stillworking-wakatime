7a4ea63367f9a7bcc6b2450410f72f10ddb80307
Jean-Christophe Hoelt <hoelt@fovea.cc>
2020-03-31 13:42:00 +0300

    Add reports

---
+++ reports/project.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os
+import time
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+db_file = os.path.expanduser('~/.stillworking.db')
+
+last_entry = None
+activities = {}
+
+def _db_process():
+    global db_file
+    with open(db_file, 'rb') as f:
+        for line in f:
+            _db_process_line(line)
+
+def _db_parse_line(line):
+    tokens = line.split('\n')[0].split(';')
+    date = tokens[0]
+    project = tokens[1]
+    entity = ';'.join(tokens[2:])
+    return [date, project, entity]
+
+def _db_process_line(line):
+    global last_entry
+    global activities
+    entry = _db_parse_line(line)
+    [next_date, next_project, next_entity] = entry
+    if last_entry:
+        [last_date, last_project, last_entity] = last_entry
+    else:
+        last_date = 0
+        last_project = ''
+        last_entity = ''
+    last_entry = entry
+    if last_project:
+        duration = int(next_date) - int(last_date)
+        if duration > 60 * 15:
+            # no reports for a long period, assuming we stopped working
+            duration = 0
+        date = datetime.utcfromtimestamp(int(last_date)).strftime('%Y-%m-%d')
+        if date:
+            key = "%s;%s;%s" % (date, last_project, last_entity)
+            activities[key] = activities.get(key, 0) + duration
+_db_process()
+
+def should_show(date, project):
+    if len(sys.argv) < 2:
+        return True
+    for i in sys.argv[1:]:
+        if not date.startswith(i) and project != i:
+            return False
+    return True
+
+total_duration = 0
+for key in sorted(activities.keys()):
+    duration = activities[key]
+    [date, project, entity] = key.split(';')
+    if should_show(date, project):
+        total_duration += duration
+        duration_fmt = str(timedelta(seconds=duration))[:-3]
+        print "%10s [%5s] %15s: %s" % (date, duration_fmt, project, entity)
+
+duration_fmt = str(timedelta(seconds=total_duration))[:-3]
+print "      TOTAL:%5s " % duration_fmt
